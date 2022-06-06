cbc6e43767472589fbfca9caebefa5fba7dba99b
Esther <qordmsql2000@gmail.com>
2022-06-06 13:42:26 -0400

    Server that increments every request

--- basicS.go
+++ basicS.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
-func main() {
+var counter int
+var mutex = &sync.Mutex{}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
+	http.HandleFunc("/", echoString)
+
+	http.HandleFunc("/increment", incrementCounter)
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
