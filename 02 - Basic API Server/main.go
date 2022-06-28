bd05230ece7a59419aef48e4bf023092dc123175
Esther <qordmsql2000@gmail.com>
2022-06-28 15:46:33 -0400

    Made unit tests and made subtle changes to main.go

--- 02 - Basic API Server/main.go
+++ 02 - Basic API Server/main.go
@@ -2,13 +2,102 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	MBTI  string `json:"mbti"`
+}
+
+var Users []User
+
+func getUsers() []User {
+	return Users
+}
+
+func returnAllUsersHandler(w http.ResponseWriter, r *http.Request) {
+	result, err := json.Marshal(getUsers())
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
+	w.WriteHeader(http.StatusOK)
+}
+
+func getSingleUser(id int) (*User, error) {
+	for _, user := range Users {
+		if user.Id == id {
+			return &user, nil
+		}
+	}
+	return nil, errors.New("no such user exists")
+}
+
+func returnSingleUserHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	user, err := getSingleUser(id)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	result, err := json.Marshal(user)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
+	w.WriteHeader(http.StatusOK)
+}
+
+func createNewUser(user User) {
+	Users = append(Users, user)
+}
+
+func createNewUserHandler(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var user User
+	json.Unmarshal(reqBody, &user)
+	createNewUser(user)
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("success"))
+}
+
+func deleteUser(id int) {
+	for index, user := range Users {
+		if user.Id == id {
+			Users = append(Users[:index], Users[index+1:]...)
+		}
+	}
+}
+
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	deleteUser(id)
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("success"))
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the MBTI Test Home Page!")
@@ -17,68 +106,14 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/users", returnAllUsers)
-	myRouter.HandleFunc("/user", createNewUser).Methods("POST")
-	myRouter.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/{id}", returnSingleUser)
+	myRouter.HandleFunc("/users", returnAllUsersHandler).Methods("GET")
+	myRouter.HandleFunc("/user", createNewUserHandler).Methods("POST")
+	myRouter.HandleFunc("/user/{id}", deleteUserHandler).Methods("DELETE")
+	myRouter.HandleFunc("/user/{id}", returnSingleUserHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":3000", myRouter))
 }
 
 func main() {
-	Users = []User{
-		{Id: "1", Name: "John Smith", Email: "example1@gmail.com", MBTI: "INTP"},
-		{Id: "2", Name: "Jane Doe", Email: "example2@gmail.com", MBTI: "ENFP"},
-	}
+	Users = []User{}
 	handleRequests()
-}
-
-type User struct {
-	Id    string `json:"Id"`
-	Name  string `json:"Name"`
-	Email string `json:"Email"`
-	MBTI  string `json:"MBTI"`
-}
-
-var Users []User
-
-func returnAllUsers(w http.ResponseWriter, r *http.Request) {
-	prettyprint(w, Users)
-}
-
-func prettyprint(w http.ResponseWriter, data interface{}) {
-	enc := json.NewEncoder(w)
-	enc.SetIndent("", "    ")
-	enc.Encode(data)
-	return
-}
-
-func returnSingleUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-
-	for _, user := range Users {
-		if user.Id == key {
-			prettyprint(w, user)
-		}
-	}
-}
-
-func createNewUser(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var user User
-	json.Unmarshal(reqBody, &user)
-	Users = append(Users, user)
-
-	prettyprint(w, user)
-}
-
-func deleteUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	for index, user := range Users {
-		if user.Id == id {
-			Users = append(Users[:index], Users[index+1:]...)
-		}
-	}
 }
---
+++ 02 - Basic API Server/main_test.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestReturnAllUsersHandler(t *testing.T) {
+	Users = []User{
+		{Id: 1, Name: "John Smith", Email: "example1@gmail.com", MBTI: "INTP"},
+		{Id: 2, Name: "Jane Doe", Email: "example2@gmail.com", MBTI: "ENFP"},
+	}
+	req, err := http.NewRequest("GET", "/users", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(returnAllUsersHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := []User{
+		{Id: 1, Name: "John Smith", Email: "example1@gmail.com", MBTI: "INTP"},
+		{Id: 2, Name: "Jane Doe", Email: "example2@gmail.com", MBTI: "ENFP"},
+	}
+	exp, err := json.Marshal(expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rr.Body.String() != string(exp) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body, string(exp))
+	}
+}
+
+func TestReturnSingleUserHandler(t *testing.T) { //ask how to set specific url param
+	Users = []User{
+		{Id: 1, Name: "John Smith", Email: "example1@gmail.com", MBTI: "INTP"},
+		{Id: 2, Name: "Jane Doe", Email: "example2@gmail.com", MBTI: "ENFP"},
+	}
+	req, err := http.NewRequest("GET", "/user/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/user/{id}", returnSingleUserHandler).Methods("GET")
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := &User{
+		Id: 1, Name: "John Smith", Email: "example1@gmail.com", MBTI: "INTP",
+	}
+
+	exp, err := json.Marshal(expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want, err := getSingleUser(1)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if !(reflect.DeepEqual(want, expected)) {
+		t.Errorf("Didn't get correct user, got %v want %v", want, expected)
+	}
+	if rr.Body.String() != string(exp) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body, string(exp))
+	}
+}
+
+func TestCreateNewUserHandler(t *testing.T) { //create 했을때 getalluser 통해 확인할수있는지 아니면 func자체에 넣는지
+	inputUser := User{Id: 3, Name: "Test", Email: "test@gmail.com", MBTI: "ISFJ"}
+
+	result, err := json.Marshal(inputUser)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	req, err := http.NewRequest("POST", "/user", bytes.NewBuffer(result))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(createNewUserHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+	expected := &User{Id: 3, Name: "Test", Email: "test@gmail.com", MBTI: "ISFJ"}
+
+	want, err := getSingleUser(3)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if !(reflect.DeepEqual(want, expected)) {
+		t.Errorf("Didn't create user, got %v want %v", want, expected)
+	}
+	if rr.Body.String() != "success" {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body, "success")
+	}
+}
+
+func TestDeleteUserHandler(t *testing.T) { //ask how to set specific url param
+	Users = []User{
+		{Id: 1, Name: "John Smith", Email: "example1@gmail.com", MBTI: "INTP"},
+		{Id: 2, Name: "Jane Doe", Email: "example2@gmail.com", MBTI: "ENFP"},
+	}
+	req, err := http.NewRequest("DELETE", "/user/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/user/{id}", deleteUserHandler).Methods("DELETE")
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusAccepted {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusAccepted)
+	}
+
+	want, err := getSingleUser(1)
+	// want no such user error
+	if err == nil {
+		t.Errorf(err.Error())
+	}
+
+	if want != nil {
+		t.Errorf("Didn't delete user, got %v want %v", want, nil)
+	}
+
+	if rr.Body.String() != "success" {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body, "success")
+	}
+}
